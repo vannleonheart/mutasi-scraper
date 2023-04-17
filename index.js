42fe433723f7dd699cf65de4d28e007fb92dc47e
taqin <abdulmuttaqin456@gmail.com>
2023-04-17 18:45:10 +0700

    fixed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports = {
-    ScrapBCA : require('./lib/bank/BCA.class.js')
+    ScrapBCA : require('./lib/bank/BCA.class.js'),
+    ScrapMandiri : require('./lib/bank/MANDIRI.class.js'),
 }
