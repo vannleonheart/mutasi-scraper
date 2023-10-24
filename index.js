aacd7f66494af02f9cfdd6f2e1757dc2692a2d99
taqin <abdulmuttaqin456@gmail.com>
2023-10-24 12:59:45 +0700

    update 1 file and delete 2 files

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = {
     ScrapBCA : require('./lib/bank/BCA.class.js'),
     ScrapBNI : require('./lib/bank/BNI.class.js'),
+    ScrapMCM : require('./lib/bank/MCM.class.js'),
+    ScrapBRI : require('./lib/bank/BRI.class.js'),
 }
